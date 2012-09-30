44baffc167e1b6fee604313ff9f274d8d5b7ee02
Tim <t@imothee.com>
2012-09-30 14:15:28 +1000

    Fixes License Typo

--- StegView.m
+++ StegView.m
@@ -18,7 +18,7 @@
 //  GNU General Public License for more details.
 // 
 //  You should have received a copy of the GNU General Public License
-//  along with TmpDisk.  If not, see <http://www.gnu.org/licenses/>.
+//  along with ImageSecrets.  If not, see <http://www.gnu.org/licenses/>.
 
 #import "StegView.h"
 
